5f9452e20414eb3ef240da5ad37aad90bf52f1b9
Ollie <ohermans1@gmail.com>
2021-12-13 12:42:57 +1100

    Calc sass added

--- calculator/script.js
+++ calculator/script.js
@@ -28,6 +28,7 @@ const saveNumbers = (value) => {
     number2 = number2 + value;
     screenNumbers.innerHTML = number1 + " " + inUseOperator + " " + number2;
   }
+  sassBot();
 };
 
 const operator = (value) => {
@@ -45,9 +46,8 @@ const sum = () => {
   }
   answer = math.evaluate(number1 + inUseOperator + number2);
   screenNumbers.innerHTML = number1 + " " + inUseOperator + " " + number2 + " = " + answer;
-  number1 = answer;
-  number2 = "";
-  inUseOperator = "";
+  sassBot();
+  resetAfterCalc();
 };
 
 const clear = (value) => {
@@ -63,6 +63,7 @@ const decimal = (value) => {
   if (number2 === "") {
     if (number1.includes(".")) {
       screenChat.innerHTML = "You can't do more then 1 decimal stupid";
+      frustrationMeter++;
     } else {
       number1 = number1 + value;
       screenNumbers.innerHTML = number1;
@@ -70,6 +71,7 @@ const decimal = (value) => {
   } else {
     if (number2.includes(".")) {
       screenChat.innerHTML = "You can't do more then 1 decimal stupid";
+      frustrationMeter++;
     } else {
       number2 = number2 + value;
       screenNumbers.innerHTML = number1 + " " + inUseOperator + " " + number2;
@@ -79,23 +81,103 @@ const decimal = (value) => {
 
 const sassBot = () => {
   if (answer <= 20 && answer != "") {
-    screenChat.innerHTML = "Seriously... do you really need a calculator for that?";
+    if (frustrationMeter === 1) {
+      screenChat.innerHTML = "Seriously... do you really need a calculator for that?";
+      frustrationMeter++;
+    } else if (frustrationMeter === 2) {
+      screenChat.innerHTML = "You are really wasting my time!";
+      frustrationMeter++;
+    } else if (frustrationMeter === 3) {
+      screenChat.innerHTML = "Please... can you just work this out in your head";
+      frustrationMeter++;
+    } else if (frustrationMeter === 4) {
+      screenChat.innerHTML = "Do your own bloody calculations!";
+      screenNumbers.innerHTML = number1 + " " + inUseOperator + " " + number2 + " = " + "Piss off";
+      frustrationMeter++;
+    } else {
+      screenNumbers.innerHTML = "Nope";
+      screenChat.innerHTML = "*Silent treatment*";
+      frustrationMeter++;
+    }
   }
+
+  if (answer > 20 && answer <= 100 && answer != "") {
+    if (frustrationMeter === 1) {
+      screenChat.innerHTML = "Fine, I can work that out.";
+      frustrationMeter++;
+    } else if (frustrationMeter === 2) {
+      screenChat.innerHTML = "Again? Why are you doing so many calculations?";
+      frustrationMeter++;
+    } else if (frustrationMeter === 3) {
+      screenChat.innerHTML = "I'm sick of this, go get a proper calculator!";
+      frustrationMeter++;
+    } else if (frustrationMeter === 4) {
+      screenChat.innerHTML = "No, I refuse!";
+      screenNumbers.innerHTML = number1 + " " + inUseOperator + " " + number2 + " = " + "Piss off";
+      frustrationMeter++;
+    } else {
+      screenNumbers.innerHTML = "Nope";
+      screenChat.innerHTML = "*Silent treatment*";
+      frustrationMeter++;
+    }
+  }
+
+  if (answer > 100 && answer != "") {
+    if (frustrationMeter === 1) {
+      screenChat.innerHTML = "I'll try, but these are pretty big numbers!";
+      frustrationMeter++;
+    } else if (frustrationMeter === 2) {
+      screenChat.innerHTML = "Wow, really pushing me today";
+      frustrationMeter++;
+    } else if (frustrationMeter === 3) {
+      screenChat.innerHTML = "Come on, please I'm worn out from all these calculations";
+      frustrationMeter++;
+    } else if (frustrationMeter === 4) {
+      screenChat.innerHTML = "I can't do it, I don't know the answer";
+      screenNumbers.innerHTML = number1 + " " + inUseOperator + " " + number2 + " = " + "???";
+      frustrationMeter++;
+    } else {
+      screenNumbers.innerHTML = "Nope";
+      screenChat.innerHTML = "*Silent treatment*";
+      frustrationMeter++;
+    }
+  }
+
+  if (number1.length === 3 || number2.length === 3) {
+    screenChat.innerHTML = "This seems like a pretty big number, but I'll manage!";
+  } else if (number1.length === 4 || number2.length === 4) {
+    screenChat.innerHTML = "This is getting silly now, I'm not a fancy calculator.";
+  } else if (number1.length === 5 || number2.length === 5) {
+    screenChat.innerHTML = "Umm... no idea how well this is going to work!";
+  } else if (number1.length > 5 || number2.length > 5) {
+    screenChat.innerHTML = "You are on your own...";
+  }
+};
+
+const resetAfterCalc = () => {
+  number1 = answer;
+  answer = "";
+  number2 = "";
+  inUseOperator = "";
 };
 
 //!! Code
 var calculatorContainer = document.getElementById("calculator");
 var screenNumbers = document.getElementById("screenNumbers");
 var screenChat = document.getElementById("screenChat");
+var completeScreen = document.getElementById("completeScreen");
 let answer = "";
 let operators = ["+", "*", "-", "/"];
 let inUseOperator = "";
 let number1 = "";
 let number2 = "";
+let frustrationMeter = 1;
 
 calculatorContainer.addEventListener("click", (e) => {
   takeInputs(e);
-  sassBot();
+  if (frustrationMeter >= 7) {
+    completeScreen.style.backgroundColor = "black";
+  }
 });
 
 /* Pseudo Code
